a71d5cea3eb3be442d55be17257db22eecfb1161
akiburrohman <115100973+akiburrohman@users.noreply.github.com>
2026-01-17 09:56:39 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -126,7 +126,8 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         rows = cur.fetchall()
         con.close()
 
-        
+        def escape_md1(text):
+        return str(text).replace("_", "\\_").replace("*", "\\*").replace("[", "\\[").replace("`", "\\`")
 
         msg = "📊 Users Stats:\n\n"
         for r in rows:
@@ -266,5 +267,6 @@ if __name__ == "__main__":
     init_db()
     threading.Thread(target=run_flask, daemon=True).start()
     run_bot()
+
 
 
